92ae3cce2cc618a80718d57880c93ded09113041
shiqiangli <lishiqiangweb@163.com>
2015-07-27 20:04:55 +0800

    Change the source code to match MVC design model, Lecture 3 in Stanford Open Course. Nothing changed in storyboard.

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -13,30 +13,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleTypingANumber: Bool = false
-    var dotCount = 0
+    var brain = CalculatorBrain()
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        if digit == "." {
-            dotCount++
-        }
-    
+        
         if userIsInTheMiddleTypingANumber {
-            if dotCount > 1 && digit == "." {
-                display.text = display.text!
-            } else {
-                display.text = display.text! + digit
-            }
+            display.text = display.text! + digit
         } else {
-            if digit == "." { // for type ".1234", display "0.1234"
-                display.text = "0" + digit
-            } else {
-                display.text = digit
-            }
+            display.text = digit
             userIsInTheMiddleTypingANumber = true
         }
-    }
         
+    }
+    
     @IBAction func backSpace() {
         if !display.text!.isEmpty {
             clear()
@@ -48,42 +38,23 @@ class ViewController: UIViewController {
         if userIsInTheMiddleTypingANumber {
             enter()
         }
-        switch operation {
-        case "×": performOperation { $0 * $1 }
-        case "÷": performOperation { $1 / $0 }
-        case "+": performOperation { $0 + $1 }
-        case "×": performOperation { $1 - $0 }
-        case "√": performOperation { sqrt($0) }
-        case "sin": performOperation { sin($0) }
-        case "cos": performOperation { cos($0) }
-        case "π": displayValue = M_PI
-            enter()
-        default:break
-        }
-    }
-    
-    func performOperation(operation: (Double, Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    private func performOperation(operation: Double -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
         }
     }
     
     @IBAction func enter() {
         userIsInTheMiddleTypingANumber = false
-        dotCount = 0
-        operandStack.append(displayValue)
-        println("operandStack = \(operandStack)")
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
     }
-
-    var operandStack = Array<Double>()
     
     var displayValue: Double {
         get {
@@ -98,8 +69,7 @@ class ViewController: UIViewController {
     @IBAction func clear() {
         userIsInTheMiddleTypingANumber = false
         display.text = "0"
-        dotCount = 0
-        operandStack.removeAll(keepCapacity: false)
+        brain.clear()
     }
 }
 
