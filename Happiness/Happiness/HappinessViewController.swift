f6357555be0058502f08aa6e8622ebdbee52c1d8
shiqiangli <lishiqiangweb@163.com>
2015-08-11 09:56:19 +0800

    Lecture 5 View introduction of iOS

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by lishiqiang on 15/8/11.
+//  Copyright (c) 2015年 lishiqiang. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
